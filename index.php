9c59eac205b7fe69c62c79c4ecc9a2e7c128412e
Kazi Emrul Kayes Emon <kek.emon2k8@gmail.com>
2020-12-30 20:10:09 +0600

    Logout action

--- index.php
+++ index.php
@@ -64,7 +64,7 @@ a, p {
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Logout</button>
+      <button onclick="location.href='logout.php';" class="btn btn-outline-success my-2 my-sm-0" type="submit">Logout</button>
     </form>
   </div>
 </nav>
